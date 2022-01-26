c702dd2ade436f99d58899f5865ca0d7d74cbef6
Julia <riyamtechcampus@gmail.com>
2022-01-26 10:46:42 -0800

    add models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
+from webbrowser import register
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer , Product , Cart , CartItems , ShippingAddress
+
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItems)
+admin.site.register(ShippingAddress)
